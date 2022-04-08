6b16d0d50283f0a690df2a0214fecbba5bd8cd32
Talesantos <talesantos7@gmail.com>
2022-04-08 16:15:33 -0300

    Ajusta short tag de impressão do php

--- layouts/parts/form-communication-button-footer.php
+++ layouts/parts/form-communication-button-footer.php
@@ -2,5 +2,5 @@
 use MapasCulturais\i;
 ?>
 <div class="box" style="color:black !important">
-<a class="btn btn-accent btn-large" href="<?=$url?>"><? i::_e("Acesse o Formulário de Comunicação", 'FormCommunication')?></a> 
+<a class="btn btn-accent btn-large" href="<?=$url?>"><?= i::_e("Acesse o Formulário de Comunicação", 'FormCommunication')?></a> 
 </div>
--- layouts/parts/home-developer--form-communication.php
+++ layouts/parts/home-developer--form-communication.php
@@ -6,7 +6,7 @@ use MapasCulturais\i;
             <div class="columns is-centered">
                 <div class="column is-half">
             
-                    <h1 class="title has-text-centered color-white"><? i::_e("Caixa de Comunicação", 'FormCommunication')?></h1>
+                    <h1 class="title has-text-centered color-white"><?= i::_e("Caixa de Comunicação", 'FormCommunication')?></h1>
                     <?php if(isset($_SESSION['form_communication_mess']) && $_SESSION['form_communication_mess']['type']!= ''): ?>
                     <div class="alert <?=$_SESSION['form_communication_mess']['type']?>" style="display: inline-block;"><?=$_SESSION['form_communication_mess']['menssage']?></div>
                     <?php unset($_SESSION['form_communication_mess']) ?>
@@ -14,35 +14,35 @@ use MapasCulturais\i;
 
                     <form action="<?=$url?>" method="POST">
                         <div class="field">
-                            <label class="label"><? i::_e("Nome*", 'FormCommunication')?></label>
+                            <label class="label"><?= i::_e("Nome*", 'FormCommunication')?></label>
                             <div class="control">
                                 <input name="nome" class="input" type="text" placeholder="Digite seu nome" required>
                             </div>
                         </div>
 
                         <div class="field">
-                            <label class="label"><? i::_e("E-mail*", 'FormCommunication')?></label>
+                            <label class="label"><?= i::_e("E-mail*", 'FormCommunication')?></label>
                             <div class="control">
                                 <input name="email" class="input" type="email" placeholder="Digite seu e-mail" required>
                             </div>
                         </div>
 
                         <div class="field">
-                            <label class="label"><? i::_e("Telefone", 'FormCommunication')?></label>
+                            <label class="label"><?= i::_e("Telefone", 'FormCommunication')?></label>
                             <div class="control">
                                 <input name="tel" class="input" type="tel" pattern="\([0-9]{2}\)[9]{1}[0-9]{4}-[0-9]{4}" placeholder="(xx)9xxxx-xxxx">
                             </div>
                         </div>
 
                         <div class="field">
-                            <label class="label"><? i::_e("Cidade", 'FormCommunication')?></label>
+                            <label class="label"><?= i::_e("Cidade", 'FormCommunication')?></label>
                             <div class="control">
                                 <input name="cidade" class="input" type="text" placeholder="Digite sua cidade">
                             </div>
                         </div>
 
                         <div class="field">
-                            <label class="label"><? i::_e("Mensagem*", 'FormCommunication')?></label>
+                            <label class="label"><?= i::_e("Mensagem*", 'FormCommunication')?></label>
                             <div class="control">
                                 <textarea name="mensagem" class="textarea" cols="35" placeholder="Digite sua mensagem" maxlength="3000" required></textarea>
                             </div>
@@ -50,7 +50,7 @@ use MapasCulturais\i;
 
                         <div class="field is-grouped">
                             <div class="control">
-                                <button class="button-submit"><? i::_e("Enviar", 'FormCommunication')?></button>
+                                <button class="button-submit"><?= i::_e("Enviar", 'FormCommunication')?></button>
 
                             </div>
 
